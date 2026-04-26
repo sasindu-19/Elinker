f908145603c628a75eaf8d90b841e60efffe8694
Sasindu Kaveesha <sasindukaveesha2004@gmail.com>
2026-04-26 10:09:21 +0530

    feat: implement password visibility toggle functionality in login and signup forms

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,23 @@ loginlink.onclick = () => {
 function goToSignup() {
     window.location.href = "signup.html";
 }
+
+// Toggle Password Visibility
+const passwordToggles = document.querySelectorAll('.password-toggle');
+
+passwordToggles.forEach(toggle => {
+    toggle.addEventListener('click', function() {
+        const parent = this.parentElement;
+        const passwordInput = parent.querySelector('input');
+        
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            this.classList.remove('bx-hide');
+            this.classList.add('bx-show');
+        } else {
+            passwordInput.type = 'password';
+            this.classList.remove('bx-show');
+            this.classList.add('bx-hide');
+        }
+    });
+});
